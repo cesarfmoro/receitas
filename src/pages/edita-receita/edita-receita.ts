8b787e7da476f9256f8c09009eb4f09bd25da0bd
cesarfmoro <cesarfmoro@gmail.com>
2017-04-06 17:39:00 -0300

    Criando o form de criação de rcceitas

--- src/pages/edita-receita/edita-receita.ts
+++ src/pages/edita-receita/edita-receita.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'page-edita-receita',
@@ -9,11 +10,23 @@ export class EditaReceitaPage {
  
   mode = 'Nova';
   niveisDificuldade = ['Fácil', 'Médio', 'Difícil'];
+  formReceita: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
     this.mode = this.navParams.get('mode');
+    this.iniciaForm();
+  }
 
+  private iniciaForm() {
+    this.formReceita=new FormGroup({
+      'nome': new FormControl(null, Validators.required),
+      'descricao': new FormControl(null, Validators.required),
+      'dificuldade': new FormControl('Médio', Validators.required)
+    });
+  }
+
+  envia() {
+    console.log(this.formReceita);
   }
 
 
