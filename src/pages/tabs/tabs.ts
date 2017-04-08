09f8a4a4f8927a240852632324b1fad8ce8a5878
cesarfmoro <cesarfmoro@gmail.com>
2017-04-08 14:30:09 -0300

    Pequenos ajustas

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ListaComprasPage } from "../lista-compras/lista-compras";
 import { ReceitasPage } from "../receitas/receitas";
 
-
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
